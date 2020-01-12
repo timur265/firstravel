788b464c61322c12d5084bd56726f6c7261ce164
Timur <45308683+timur265@users.noreply.github.com>
2020-01-12 10:47:25 +0500

    Update watches.php

--- app/views/layouts/watches.php
+++ app/views/layouts/watches.php
@@ -14,7 +14,6 @@
     <link rel="stylesheet" href="vendor/owl.carousel.min.css">
     <link rel="stylesheet" href="vendor/owl.theme.default.min.css">
     <link rel="stylesheet" href="css/main.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 
 
 </head>
